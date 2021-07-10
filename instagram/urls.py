e0558765191b69824ac2ef0c2c82d8d13fc6ee2d
Your Name <you@example.com>
2021-07-10 13:35:11 +0300

    created the login page

--- instagram/urls.py
+++ instagram/urls.py
@@ -21,5 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('authy.urls')),
-] 
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
