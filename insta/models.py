6a669c0ba118936c2cbd6d0c75be36423ce842a4
Your Name <you@example.com>
2021-07-11 06:27:19 +0300

    created a tags class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your models here.
+
+def user_directory_path(instance,filename):
+    #this fle will be uploaded to MEDIA_ROOT /user()/filename
+    return 'user_{0}/{1}'.format(instance.user.id,filename)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=75,verbose_name='Tag')
+    slug = models.SlugField(null=False, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Tags'
+    def get_absolute_url(self):
+        return reverse('tags', args=[self.slug])
+    def __str__(self):
+        self.title
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save()
