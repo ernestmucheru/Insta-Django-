4e43a4bb23fc89daad3eee0fb917a2d388f8e671
Your Name <you@example.com>
2021-07-11 06:57:49 +0300

    updated the admin page with Post,Tag and Follow models

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from insta.models import Post, Tag, Follow
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Follow)
