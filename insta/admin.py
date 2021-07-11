fcc9c6bfe161b9be441a993375e2f28a23d24c59
Your Name <you@example.com>
2021-07-11 07:20:08 +0300

    added stream model to admin panel

--- insta/admin.py
+++ insta/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from insta.models import Post, Tag, Follow
+from insta.models import Post, Tag, Follow, Stream
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Follow)
+admin.site.register(Stream)
